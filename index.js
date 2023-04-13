d2fee57901a9a0afbc78bf16578f49189ca650d1
itsmrTech <Shamot.group@gmail.com>
2023-04-13 11:28:50 +0330

    Attacker functionality created

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const generateRandom = (min, max) => {
 
 //Total number of mines to be planted
 const minesCount = 10
-
+//Range of Attack
+const range = 2;
 let playground = new Array(minesCount).fill('')
     .map(a => new Array(minesCount).fill(' '));
 console.table(playground)
@@ -31,3 +32,49 @@ const plantBombs=()=>{
 }
 plantBombs();
 
+const actions = [];
+let totalDefusedBombsCount = 0;
+let attacksCount = 0;
+
+const attack = () => {
+    const attackIndex = generateRandom(0, minesCount * minesCount);
+    let foundIndex = filledIndices.findIndex(ind => attackIndex === ind)
+    //If the index is filled before, retry
+    if (foundIndex !== -1) return attack();
+
+    //Export 2D indices from 1D index
+    let j = Math.floor(attackIndex / minesCount)
+    let k = attackIndex % minesCount;
+    //Flag the index as Attacked.
+    playground[j][k] = 'A'
+
+    //Find surrounding objects of the attacked index.
+    for (let counter1 = j - range; counter1 <= j + range; counter1++) {
+        for (let counter2 = k - range; counter2 <= k + range; counter2++) {
+            if (counter1 >= 0 && counter2 >= 0 && counter1 < playground.length && counter2 < playground[counter1].length) {
+
+                //If there is a bomb, defuse it.
+                if (playground[counter1][counter2] === 'B') {
+                    playground[counter1][counter2] = 'X'
+                    totalDefusedBombsCount++;
+                }
+                //If there is nothing in the index, flag it as discovered.
+                else if (playground[counter1][counter2] === ' ')
+                    playground[counter1][counter2] = '#'
+
+                filledIndices.push((counter1*minesCount)+counter2);
+
+            }
+        }
+    }
+    attacksCount++;
+    const action = { attack: attacksCount, location: [j, k], bombs: totalDefusedBombsCount };
+
+    actions.push(action)
+    console.log(">> Step Attack",action.attack,":",action.location)
+    console.table(playground);
+    console.log(actions);
+}
+while (totalDefusedBombsCount < minesCount) {
+    attack();
+}
